b04156469d846956e3b3a4a0e5be53a598f3208d
Florent Ruard-Dumaine <atalargo@gmail.com>
2015-03-11 18:08:04 +0100

    Provide FormBuilder helper

--- app/helpers/file_input_helper.rb
+++ app/helpers/file_input_helper.rb
@@ -1,6 +1,17 @@
+# Provide helper to active boostrap File input capacity on for file parameter
 module FileInputHelper
+    # Extend FormBuilder to provide bootstrap_fileinput helper
+    class ActionView::Helpers::FormBuilder
+        def bootstrap_fileinput(name, options)
+            options[:id] ||= "#{@object_name}_#{name}"
+            options[:name] ||= "#{@object_name}[#{name}]"
+
+            @template.bootstrap_fileinput(options)
+        end
+    end
+
     def bootstrap_fileinput(options)
-        options = options || {}
+        options ||= {}
         options_input, options_js = FileInputHelper.parse_options(options)
 
         tag(:input, options_input) <<
@@ -22,24 +33,18 @@ module FileInputHelper
         #     }')
     end
 
+    protected
+
     def self.parse_options(options)
         [parse_input_options(options), parse_js_options(options)]
     end
 
-    protected
-
     def self.parse_input_options(options)
         opts_ipt = {
-            id: :input_id,
-            type: :file,
-            class: :file,
-            multiple: false,
-            'data-preview-file-type' => :text,
-            'data-show-caption' => nil,
-            'data-show-preview' => nil,
-            accept: 'image/*'
+            id: :input_id, type: :file, class: :file, multiple: false,
+            'data-preview-file-type' => :text, 'data-show-caption' => nil,
+            'data-show-preview' => nil, accept: 'image/*'
         }
-        # opts_ipt = opts_ipt.merge(options.reject { |k, _| opts_ipt[k].nil? })
         opts_ipt.reject { |_k, v| v.nil? }.merge(options.reject { |k, _| opts_ipt[k].nil? })
     end
 
