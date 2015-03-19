394b1f4e4e18376b7273f60831391b52eb91ba9d
Florent Ruard-Dumaine <atalargo@gmail.com>
2015-03-19 19:21:15 +0100

    Switch submodules JS to Fork version during PR processw

--- app/helpers/file_input_helper.rb
+++ app/helpers/file_input_helper.rb
@@ -10,27 +10,35 @@ module FileInputHelper
         end
     end
 
+    def bootstrap_fileinput_preview(files)
+        files.map do |file|
+            image_tag(file[:url], alt: file[:name], title: file[:name], class: 'file-preview-image' )
+        end
+    end
+
     def bootstrap_fileinput(options)
         options ||= {}
         options_input, options_js = FileInputHelper.parse_options(options)
 
+        unless options_js[:initialPreview].nil?
+            previews = options_js[:initialPreview]
+            options_js.delete :initialPreview
+            options_js[:initialPreview] = bootstrap_fileinput_preview(previews)
+            options_js[:initialPreviewConfig] = previews.map do |preview|
+                { id: preview[:key], caption: preview[:name], url: preview[:delete], width: '120px' }
+            end
+        end
+
+        slugCallback = options_js[:slugCallback]
+        options_js.delete :slugCallback
+
         tag(:input, options_input) <<
-            javascript_tag("$(document).on(\'ready page:load\', function() {\
-                var opts_ips = JSON.parse('#{options_js.to_json.gsub(/'/, '\\\'')}');
-                console.log(opts_ips);
-                $('##{options_input[:id]}').fileinput(
-                    JSON.parse('#{options_js.to_json.gsub(/'/, '\\\'')}')
-                );
-            });")
-        # tag(:input,
-        #     id: :input_id,
-        #     type: :file,
-        #     class: :file,
-        #     'preview-file-type' => :text
-        # ) <<
-        #     javascript_tag('$(document).on(\'ready page:load\') {\
-        #          $("#input-id").fileinput();\
-        #     }')
+            raw("<script type=\"text/javascript\">$(document).on('ready page:load', function() {\
+                $('##{options_input[:id]}').fileinput({
+                    #{options_js.map { |k, v| "#{k}: #{v.to_json}," }.join("\n") }
+                    #{slugCallback ? 'slugCallback: ' + slugCallback : ''}
+                });
+            });</script>")
     end
 
     protected
@@ -41,19 +49,22 @@ module FileInputHelper
 
     def self.parse_input_options(options)
         opts_ipt = {
-            id: :input_id, type: :file, class: :file, multiple: false,
+            id: :input_id, name: :input_file, type: :file, class: :file, multiple: false,
             'data-preview-file-type' => :text, 'data-show-caption' => nil,
-            'data-show-preview' => nil, accept: 'image/*'
+            'data-show-preview' => nil, accept: 'image/*', 'data-show-upload' => nil
         }
-        opts_ipt.reject { |_k, v| v.nil? }.merge(options.reject { |k, _| opts_ipt[k].nil? })
+        opts_ipt.reject { |_k, v| v.nil? }.merge(options.reject { |k, _| !opts_ipt.include?(k) })
     end
 
     def self.parse_js_options(options)
         opts_js  = {
             allowedFileTypes: ['image'],
             allowedFileExtensions: [:jpg, :jpeg, :png],
-            initialCaption: ''
+            initialCaption: '', slugCallback: nil,
+            showUpload: true, uploadAsync: true, uploadUrl: nil,
+            browseClass: 'btn btn-primary btn-file', browseLabel: 'Browse...', browseIcon: nil,
+            initialPreview: nil, overwriteInitial: true, layoutTemplates: nil
         }
-        opts_js.reject { |_k, v| v.nil? }.merge(options.reject { |k, _| opts_js[k].nil? })
+        opts_js.reject { |_k, v| v.nil? }.merge(options.reject { |k, _| !opts_js.include?(k) })
     end
 end
